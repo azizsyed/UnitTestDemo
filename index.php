2ccdc22526937ff6c9b20cf4f9225260ecf76418
Aziz Syed <aziz.scarn@gmail.com>
2012-10-02 11:21:55 -0400

    Add landing page for navigation
    Like a sitemap

---
+++ index.php
@@ -0,0 +1,36 @@
+<?
+?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=1024" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<title>Unit Testing</title>
+		
+		<link href='http://fonts.googleapis.com/css?family=Advent+Pro:400,100,200,300,500,600,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<style>
+			body{
+				font-family:"Advent Pro";
+				font-size:16px;
+			}
+			a,
+			a:visited{
+				color:orange;
+				font-weight:bold;
+				text-decoration:none;
+			}
+			a:focus,
+			a:hover{
+				color:orangered;
+			}
+		</style>
+	</head>
+
+	<body>
+		<p><a href="presentation/intro.php">CRD Presentation</a></p>
+		<p><a href="presentation/">Unit Test Presentation</a></p>
+		<p><a href="site/">Demo Site</a></p>
+		<p><a href="test/">Demo Unit Tests</a></p>
+	</body>
+</html>
